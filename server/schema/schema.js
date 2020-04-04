b83aa76e8bb522de98a43f0d4810dac006e993cb
Bas <basvandriel94@gmail.com>
2020-04-04 19:51:06 +0200

    Setup next with graphql server

---
+++ server/schema/schema.js
@@ -0,0 +1,24 @@
+const graphql = require('graphql');
+
+const {GraphQLObjectType, GraphQLString} = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book', fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args) {
+                // Code to get data from db
+            }
+        }
+    }
+});
